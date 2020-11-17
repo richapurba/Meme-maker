08d301d4e40f246787fc9ac8e6f906840bafd9ed
Richa Purba <richathorne@gmail.com>
2020-11-18 06:10:53 +0700

    Uploade the App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Header from "./Header"
+import Generator from "./Generator"
+
+function App() {
+    return(
+        <div>
+            <Header/>
+            <Generator/>
+        </div>
+    )
+}
+
+export default App
